f07b11e2d54949097d6ec62dca674311a8e55cde
Shubhajit <shubjitgithub@gmail.com>
2024-06-17 21:42:13 +0530

    Done

--- src/main/java/Contains.java
+++ src/main/java/Contains.java
@@ -10,7 +10,30 @@ public class Contains {
      * @param target the value that the method should be searching for.
      * @return true if arr contains target, false otherwise.
      */
-    public boolean arrayContains(int[] arr, int target){
+    public boolean arrayContains(int[] arr, int target) {
+for(int i=0;i<arr.length;i++){
+    for(int j=i;j<arr.length;j++){
+        if(arr[i]>arr[j]){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+
+        }
+    }
+}
+
+        int start = 0;
+        int end = arr.length-1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target)
+                return true;
+            else if (target < arr[mid])
+                end = mid - 1;
+            else if (target > arr[mid])
+                start = mid + 1;
+        }
         return false;
+
     }
 }
